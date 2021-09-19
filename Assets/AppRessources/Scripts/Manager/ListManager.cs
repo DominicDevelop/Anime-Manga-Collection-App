7a1b29900be6d7980fcd021b5644b2fd15e2d837
Dominic Schroeder <dominic.develop93@gmail.com>
2021-09-19 13:13:56 +0200

    Save ListElems in a Dict now

--- Assets/AppRessources/Scripts/Manager/ListManager.cs
+++ Assets/AppRessources/Scripts/Manager/ListManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
@@ -8,30 +10,52 @@ using WebP;
 public class ListManager : MonoSingleton<ListManager>
 {
     #region Constants
+    #region PATHES
     private readonly string IMAGE_CDN_PATH = "https://cdn.anisearch.de/images/{0}";
+    #endregion
+
+    #region REGEX
+    private readonly string REGEX_MANGA_ID_FROM_IMG_PATH = @"(\d+).webp";
+    #endregion
     #endregion
 
     [SerializeField]
     private Transform _searchResultList;
-    private List<GameObject> _resultListElems = new List<GameObject>();
+    private Dictionary<string, GameObject> _resultListElems = new Dictionary<string, GameObject>();
 
     private string _searchTerm = "";
+    private string _prevSearchTerm = "";
 
     public void Search() {
-        if (_searchTerm.Length < 1)
+        if (_searchTerm.Length < 1 || _prevSearchTerm.Equals(_searchTerm))
             return;
 
+        _prevSearchTerm = _searchTerm;
+
+        #region Clear Search Results
         foreach (var elem in _resultListElems) {
-            GameObject.Destroy(elem);
+            GameObject.Destroy(elem.Value);
         }
+
+        _resultListElems.Clear();
+        #endregion
 
         var listElems = HtmlParser.SendMangaSearchRequest(_searchTerm);
 
         if (listElems.Count > 0) {
             foreach (var elem in listElems) {
+                var imagePath = elem.Attributes["data-bg"].Value;
+
                 GameObject listElem = PrefabManager.Instance.GetMangaListElem(_searchResultList);
-                _resultListElems.Add(listElem);
-                StartCoroutine(SetCoverByImageURL(listElem, elem.Attributes["data-bg"].Value));
+
+                var match = Regex.Match(imagePath, REGEX_MANGA_ID_FROM_IMG_PATH);
+                string mangaId = match.Groups[0].Value.Split('.')[0];
+
+                if (!_resultListElems.ContainsKey(mangaId)) {
+                    _resultListElems.Add(mangaId, listElem);
+                }
+
+                StartCoroutine(SetCoverByImageURL(listElem, imagePath));
             }
         }
     }
